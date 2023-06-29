eceb578b07149d515c0e89d43f007255677a2953
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-06-29 13:45:29 +0900

    [Fix]UIBase修正

--- Assets/Hikanyan_Assets/Script/Hikanyan.UI/UIBase.cs
+++ Assets/Hikanyan_Assets/Script/Hikanyan.UI/UIBase.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 
-public abstract class UIBase : MonoBehaviour
+namespace Hikanyan.UI
 {
-    // 共通のUIの基底クラス
+    public abstract class UIBase : MonoBehaviour
+    {
+        // 共通のUIの基底クラス
+    }
 }
